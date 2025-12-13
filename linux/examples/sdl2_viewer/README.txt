9f4b8b6e1c0834992fde856b719ad643002d7d9c
Larry Bank <bitbank@pobox.com>
2025-12-13 15:16:27 +0000

    Updated to handle special case of progressive images

--- linux/examples/sdl2_viewer/README.txt
+++ linux/examples/sdl2_viewer/README.txt
@@ -1,6 +1,16 @@
+JPEGDEC SDL2 Viewer
+-------------------
+This example program shows how to view JPEG images with JPEGDEC and
+display them in a SDL2 window. The window waits for the user to press ESC
+or click on the close button.
+
 Requirements:
 -------------
 SDL2
-Use the following command to install it:
+Use the following command to install it on Linux:
  - sudo apt install libsdl2-dev
+
+On MacOS:
+brew install sdl2
+
 
--- linux/examples/sdl2_viewer/main.cpp
+++ linux/examples/sdl2_viewer/main.cpp
@@ -4,33 +4,53 @@
 #include <stdlib.h>
 #include <JPEGDEC.h>
 JPEGDEC jpg;
+uint8_t *pFrameBuf; // 16-bit combined frame buffer
+int w, h;
+
+int JPEGDraw(JPEGDRAW *pDraw)
+{
+    uint16_t *d, *s;
+    
+    d = (uint16_t *)pFrameBuf;
+    d += pDraw->x;
+    d += (pDraw->y * w);
+    s = pDraw->pPixels;
+    for (int y=0; y<pDraw->iHeight; y++) {
+        memcpy(d, s, pDraw->iWidth * sizeof(uint16_t));
+        d += w;
+        s += pDraw->iWidth;
+    }
+    return 1;
+} /* JPEGDraw() */
 
 int main(int argc, char *argv[])
 {
     SDL_Window *win;
     SDL_Surface *canvas, *winSurface;
     void *pOld;
-    int w, h;
-    uint8_t *pFrameBuf; // 16-bit combined frame buffer
     
     if (argc != 2) {
         printf("sdl2 jpeg viewer\nUsage: sdl2_viewer <filename>\n");
         return -1;
     }
-    if (!jpg.open(argv[1], NULL)) {
+    if (!jpg.open(argv[1], JPEGDraw)) {
     	printf("Error opening %s = %d\n", argv[1], jpg.getLastError());
     	return -1;
     }
     jpg.setPixelType(RGB565_LITTLE_ENDIAN);
     w = jpg.getWidth(); h = jpg.getHeight();
-    
+    if (jpg.getJPEGType() == JPEG_MODE_PROGRESSIVE) {
+        // N.B. Progressive mode only supports decoding the first 'scan'
+        // The resulting image is a 1/8th thumbnail
+        w >>= 3; h >>= 3;
+    }
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
     pFrameBuf = (uint8_t *)malloc(w * h * 2); // 16-bit frame
-    jpg.setFramebuffer(pFrameBuf);
+    //jpg.setFramebuffer(pFrameBuf);
 
     win = SDL_CreateWindow("JPEGDEC viewer", 100, 100, w, h, SDL_WINDOW_SHOWN);
     if (win == nullptr) {
